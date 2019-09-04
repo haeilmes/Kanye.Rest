7f3afab46d5c8788439e171baf1e431b50a85344
Hunter <haeilmes@gmail.com>
2019-09-04 09:24:07 -0500

    Commiting an API Call example

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://api.kanye.rest";
+
+            HttpClient client = new HttpClient();
+            string response = client.GetStringAsync(url).Result;
+
+            string joke2 = JObject.Parse(response).GetValue("quote").ToString();
+
+            Console.WriteLine(joke2);
+        }
+    }
+}
